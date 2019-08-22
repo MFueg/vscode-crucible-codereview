b9cd6b27f13d0ad095822506aa8e39da69d0c0f1
Matthias <matthiasfueg@gmx.de>
2019-08-22 21:25:27 +0200

    basic settings
    - formatted with prettier

--- webpack.config.js
+++ webpack.config.js
@@ -7,34 +7,34 @@ const path = require('path');
 /**@type {import('webpack').Configuration}*/
 
 const config = {
-  target: 'node',
-  entry: './src/main.ts',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'commonjs2',
-    devtoolModuleFilenameTemplate: '../[resource-path]'
-  },
-  devtool: 'source-map',
-  externals: {
-    vscode: 'commonjs vscode'
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
-  }
+	target: 'node',
+	entry: './src/main.ts',
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+		libraryTarget: 'commonjs2',
+		devtoolModuleFilenameTemplate: '../[resource-path]'
+	},
+	devtool: 'source-map',
+	externals: {
+		vscode: 'commonjs vscode'
+	},
+	resolve: {
+		extensions: [ '.ts', '.js' ]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader'
+					}
+				]
+			}
+		]
+	}
 };
 
 module.exports = config;
